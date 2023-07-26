02c4c303d36c3e393173e3a5d2982afedc55317a
zuoshuai <zuoshuai@mininglamp.com>
2023-07-26 11:06:52 +0800

    Initial Commit

---
+++ howxm-ios-swift-demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  howxm-ios-swift-demo
+//
+//  Created by 左帅 on 2023/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ howxm-ios-swift-demo/howxm_ios_swift_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  howxm_ios_swift_demoApp.swift
+//  howxm-ios-swift-demo
+//
+//  Created by 左帅 on 2023/7/26.
+//
+
+import SwiftUI
+
+@main
+struct howxm_ios_swift_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
